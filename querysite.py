8132d85a78321dcc7c7a09e1f0e54500740c5579
Gabriel Huber <huberg18@gmail.com>
2017-11-06 02:38:53 +0100

    Update to python-valve master and add some new properties

--- querysite.py
+++ querysite.py
@@ -2,15 +2,20 @@ import concurrent.futures
 import socket
 
 import flask
-import valve.source.a2s
-import valve.source.messages
+import valve.source
 
+
+
+app = flask.Flask(__name__)
+
+@app.template_filter("yesno")
 def yesno(value):
     if value:
         return "Yes"
     else:
         return "No"
 
+@app.template_filter("duration")
 def format_duration(total_seconds):
     total_seconds = int(total_seconds)
     hours, remainder = divmod(total_seconds, 3600)
@@ -21,8 +26,6 @@ def format_duration(total_seconds):
         return "{}m {}s".format(minutes, seconds)
     else:
         return "{}s".format(seconds)
-
-app = flask.Flask(__name__)
 
 @app.route("/")
 def redirect_index():
@@ -42,8 +45,7 @@ def query():
     elif " " in server_arg:
         ip, port_str = server_arg.split(None, 1) # Split on whitespace
     else:
-        return flask.render_template("query.html", status="Error",
-            error="Port is missing.", server=server_arg), 400
+        ip, port_str = server_arg, "27015"
 
     try:
         port = int(port_str)
@@ -62,8 +64,8 @@ def query():
             error="Invalid server address.", server=server_arg), 400
 
     # We need 2 queriers because requests are not thread safe
-    info_querier = valve.source.a2s.ServerQuerier((ip, port), timeout=3)
-    players_querier = valve.source.a2s.ServerQuerier((ip, port), timeout=3)
+    info_querier = valve.source.ServerQuerier((ip, port), timeout=3)
+    players_querier = valve.source.ServerQuerier((ip, port), timeout=3)
     with concurrent.futures.ThreadPoolExecutor(max_workers=2) as pool:
         info_future = pool.submit(info_querier.info)
         players_future = pool.submit(players_querier.players)
@@ -72,43 +74,32 @@ def query():
     info_except = info_future.exception()
     players_except = players_future.exception()
 
-    if type(info_except) == valve.source.a2s.NoResponseError:
+    if type(info_except) == valve.source.NoResponseError:
         return flask.render_template("query.html", status="Error",
             error="Server did not respond.", server=server_arg), 200
-    if type(info_except) == valve.source.messages.BrokenMessageError:
+    elif type(info_except) == valve.source.BrokenMessageError:
         return flask.render_template("query.html", status="Error",
             error="Server sent a broken response.", server=server_arg), 200
     elif info_except is not None:
         raise info_except
 
     info_res = info_future.result()
-    info = dict(info_res)
-    info["password_protected"] = yesno(info_res["password_protected"])
-    info["vac_enabled"] = yesno(info_res["vac_enabled"])
 
-    if type(players_except) == valve.source.a2s.NoResponseError:
+    if type(players_except) == valve.source.NoResponseError:
         return flask.render_template("query.html", status="InfoOnly",
-            info=info, error="Server did not respond.",
+            info=info_res, error="Server did not respond.",
             server=server_arg), 200
-    if type(players_except) == valve.source.messages.BrokenMessageError:
+    elif type(players_except) == valve.source.BrokenMessageError:
         return flask.render_template("query.html", status="InfoOnly",
-            info=info, error="Server sent a broken response.",
+            info=info_res, error="Server sent a broken response.",
             server=server_arg), 200
     elif players_except is not None:
         raise players_except
 
-    players_res = players_future.result()
-    players = []
-    for player_entry in players_res["players"]:
-        player = dict(player_entry)
-        if not player["name"].strip():
-            continue
-        player["duration"] = format_duration(player_entry["duration"])
-        players.append(player)
-    players.sort(key=lambda p: p["score"], reverse=True)
+    players_res = players_future.result().players
 
     return flask.render_template("query.html", status="Success",
-        info=info, players=players, server=server_arg), 200
+        info=info_res, players=players_res, server=server_arg), 200
 
 @app.errorhandler(500)
 def server_error(e):
